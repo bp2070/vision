ef08611420b10bdd83a5dd8f3df58968ce5b5fac
bryanp <bp2070@gmail.com>
2011-09-21 15:35:58 -0400

    implemented label function logic. also added grid class to make managing the grids easier than manipulating 2d arrays manually.

--- vision.js
+++ vision.js
@@ -9,7 +9,28 @@ var context;
 
 //2d array of integers, tracks which cells are empty (0) or filled (1)
 //filled cells represent holes
-var cells;
+//var cells;
+var grid;
+
+function Grid(r, c){
+    this.rows = r;
+    this.cols = c;
+    this.cells = new Array(this.cols);
+    for (var i = 0; i < this.cols; i++){
+        this.cells[i] = new Array(this.rows);
+        for(var j = 0; j < this.rows; j++){
+            this.cells[i][j] = 0;
+        }    
+    }
+
+    this.each = function(x) {
+        for (var i = 0; i < this.cols; i++){
+            for (var j = 0; j < this.rows; j++){
+                x(i, j);
+            }
+        }
+    }
+}
 
 function init(){    
     //set number of rows, number of columns, cell width, cell height
@@ -26,7 +47,8 @@ function init(){
     //get context
     context = canvas.getContext("2d");
         
-    initCells();                
+    grid = new Grid(num_rows, num_cols);                
+    redraw();
 }
 
 function update(rows, cols, c_width, c_height){
@@ -36,17 +58,6 @@ function update(rows, cols, c_width, c_height){
     cell_height = c_height;
     pixel_width = 1 + (num_cols * cell_width);
     pixel_height = 1 + (num_rows * cell_height);
-}
-
-function initCells(){
-    cells = new Array(num_cols);
-    for (var i = 0; i < num_cols; i++){
-        cells[i] = new Array(num_rows);
-        for(var j = 0; j < num_rows; j++){
-            cells[i][j] = 0;
-        }    
-    }
-    redraw();
 }
 
 function onClick(e){
@@ -71,10 +82,10 @@ function onClick(e){
     col = Math.floor(y/cell_height);
 
     //switch cell from empty to filled or vice versa
-    if(cells[row][col] == 0){
-        cells[row][col] = 1;
+    if(grid.cells[row][col] == 0){
+        grid.cells[row][col] = 1;
     }
-    else cells[row][col] = 0;
+    else grid.cells[row][col] = 0;
 
     redraw();
 }
@@ -108,7 +119,7 @@ function drawGrid(){
 function drawCells(){
     for(var i = 0; i < num_cols; i++){
         for(var j = 0; j < num_rows; j++){
-            if(cells[i][j] == 1){
+            if(grid.cells[i][j] == 1){
                 context.fillRect(i * cell_width, j * cell_height, cell_width, cell_height);
             }                  
         }
@@ -121,7 +132,7 @@ function countHoles () {
     
     for (var i = 0; i < num_cols-1; i++){
         for(var j = 0; j < num_rows-1; j++){
-            var temp = cells[i][j] + cells[i][j+1] + cells[i+1][j] + cells[i+1][j+1];
+            var temp = grid.cells[i][j] + grid.cells[i][j+1] + grid.cells[i+1][j] + grid.cells[i+1][j+1];
         
             if (temp == 3){
                 internal_corners++;
@@ -137,27 +148,70 @@ function countHoles () {
     alert("Number of holes: " + num_holes);
 }
 
+function find(i, parent_arr){
+    while (parent_arr[i] != 0){
+        i = parent_arr[i];
+    }
+    return i;
+}
+
+function union(x, y, parent_arr){
+    j = find(x, parent_arr);
+    k = find(y, parent_arr);
+    if (j != k){
+        parent_arr[k] = j;
+    }
+}
+
 function labelHoles(){
-    for(var i = 0; i < num_rows; i++){
-        for(var j = 0; j < num_cols; j++){
-            if(cells[i][j] == 1){
-                //a = prior neighbors
-                //if(isEmpty(a)) m = ++label
-                //else m = min(labels(a))
-                //LB[i][j] = m
-                //for x in labels(a) and x != m
-                //  union(m, x, parent)
-                //end
+    var label_grid = new Grid(num_rows, num_cols);
+    var label = 0;
+    var parent_arr = new Array();
+    parent_arr.push(0);
 
+    label_grid.each(function(i, j) {
+        if(grid.cells[i][j] == 1){
+            //prior neighbors
+            var prior_neighbors = new Array(4);
+            prior_neighbors[0] = label_grid.cells[i-1][j-1];
+            prior_neighbors[1] = label_grid.cells[i][j-1];
+            prior_neighbors[2] = label_grid.cells[i+1][j-1];
+            prior_neighbors[3] = label_grid.cells[i-1][j];
+
+            //if prior_neighbors is empy m = ++label
+            var empty = true;
+            var min = 0;
+            for(var k = 0; k < 4; k++){
+                var x= prior_neighbors[k];
+               if (x != 0){
+                   if (x != label){
+                       parent_arr[x] = label;
+                   }
+                   empty = false;
+               }
+               else if (min == 0 || x < min){
+                   min = x;
+               }
+            }
+            if(empty){
+                parent_arr.push(0);
+                min = label;
+                label++;
+            }
+            label_grid.cells[i][j] = min;
+
+            for(var k = 0; k < 4; k++){
+                var x = prior_neighbors[k];
+                if (x != min){
+                    union(min, x, parent_arr);
+                }
             }
         }
-    }
+    });
 
-    for(var i = 0; i < num_rows; i++){
-        for(var j = 0; j < num_cols; j++){
-            if(cells[i][j] == 1){
-                //LB[i][j] = find(LB[i][j], parent)
-            }
+    label_grid.each(function(i, j) {
+        if(grid.cells[i][j] == 1){
+            label_grid.cells[i][j] = find(label_grid.cells[i][j], parent_arr);
         }
-    }
+    });
 }
